6ceaaae0138c9951fc0673070972d3217a1235cc
juloperag <124819739+juloperag@users.noreply.github.com>
2024-02-28 18:37:26 -0500

    clase
    se agrega elementos dentro de la clase.

--- Actividad 2/PruebaFiguras/src/main/java/poo/pruebafiguras/Circulo.java
+++ Actividad 2/PruebaFiguras/src/main/java/poo/pruebafiguras/Circulo.java
@@ -9,5 +9,19 @@ package poo.pruebafiguras;
  * @author julil
  */
 public class Circulo {
-    
+    //Definimos el atributo radio
+    int radio; 
+    //Costruccion para la clase circulo
+    Circulo(int radio) {
+    this.radio = radio;
+    }
+    }
+    //Aqui definimos los metodos de la clase
+    double calcularArea(){
+    return Math.PI*Math.pow(radio,2);
+    }
+
+    double calcularPerímetro() {
+    return 2*Math.PI*radio;
+    } 
 }
