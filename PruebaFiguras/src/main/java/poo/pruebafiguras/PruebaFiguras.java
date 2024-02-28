a35bbdeb3fd7659e3c0b32cb58be2125936db3f7
juloperag <124819739+juloperag@users.noreply.github.com>
2024-02-28 18:31:00 -0500

    Prueba_Actividad_2
    Se realiza una prueba de creacion de proyecto

---
+++ PruebaFiguras/src/main/java/poo/pruebafiguras/PruebaFiguras.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.pruebafiguras;
+
+/**
+ *
+ * @author julil
+ */
+public class PruebaFiguras {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
