febf3d01d84ddecb8b51ba51567a22fbfb12b8e7
juloperag <124819739+juloperag@users.noreply.github.com>
2024-08-04 15:45:59 -0500

    Actualizacion de ejercicio 10
    se actualiza el nombre de los atributos del ejercicio 10

--- Actividad 3/Cap4_Ejercicio10_GUI/src/logical/Estudiante.java
+++ Actividad 3/Cap4_Ejercicio10_GUI/src/logical/Estudiante.java
@@ -7,31 +7,34 @@ package logical;
 public class Estudiante {
   
     //Definimos deatributo
-    int ni;
-    String nombre;
-    double patrimonio;
-    int estrato_social;
-    private double pag_Matricula;
+	//Definimos los atributos socioeconomicos del Estudiante;
+	//Numero de inscripcion
+	int ni;
+	String nom;
+	double pat;
+	int est;
+	private double pagMat;
+
 
     //Definicion del contructor
     public Estudiante(){    
     }
     public Estudiante(int ni, String nombre, double patrimonio, int estrato_social) {
         this.ni = ni;
-        this.nombre = nombre;
-        this.patrimonio = patrimonio;
-        this.estrato_social = estrato_social;
+        this.nom = nombre;
+        this.pat = patrimonio;
+        this.est = estrato_social;
     }
     
     //Definimos los metodos
     //Metodo del calculo de la matricula
     public double calcMat() {
         //Valor minimo de matricula
-        pag_Matricula=50000;
+        pagMat=50000;
         //Excedente dependiente del estado socioeconomico
-        if(patrimonio>2000000 & estrato_social>3){
-            pag_Matricula += patrimonio*0.03; //mas 3% del patrimonio
+        if(pat>2000000 & est>3){
+            pagMat += pat*0.03; //mas 3% del patrimonio
         }
-        return  pag_Matricula;
+        return  pagMat;
     }
 }
