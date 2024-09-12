a0437e085975f06924c096feb827c9a1f9c8f242
juloperag <124819739+juloperag@users.noreply.github.com>
2024-09-11 19:09:51 -0500

    Desarrollo Actividad 6
    Se desarrolla la aplicacion de la actividad 6

---
+++ Actividad 6/Act6_Friends/src/friends/Act6_Friends.java
@@ -0,0 +1,14 @@
+
+package friends;
+
+
+public class Act6_Friends {
+    //Main
+    public static void main(String[] args) {
+        //Creamos instancia
+        MainForm frm = new MainForm();
+        //Mostramos la ventana de la interfaz Grafica
+        frm.setVisible(true);
+        frm.setLocationRelativeTo(null);    
+    }   
+}
